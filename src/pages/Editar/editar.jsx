313ad14c94832d5c6ecd0b66efc23916f101b98b
Fabio Menini <fabiomenini@MacBook-Pro-de-Fabio-2.local>
2023-08-24 16:15:07 -0300

    First commit

---
+++ src/pages/Editar/editar.jsx
@@ -0,0 +1,78 @@
+import React, {useState} from "react";
+import api from '../../services/api';
+import { useNavigate, useParams } from "react-router-dom";
+import styles from './styles.module.css';
+import { useEffect } from "react";
+
+const initialValue = {
+    title: '',
+    image: '',
+    sinopse: '',
+    url: '',
+};
+
+
+function Editar (){
+
+    const [values, setValues] = useState(initialValue);
+
+    const navigate = useNavigate();
+
+    const { id } = useParams();
+
+    const endereco = `/movies/${id}`;
+
+    useEffect( () =>{ 
+        if (id){
+            api.get(endereco)
+                .then( (response) => {
+                    setValues(response.data)
+                })
+        }
+    },[])
+    
+    function onSubmit(evento){
+        evento.preventDefault(); // não executa o comportamento default do formulário (o recarregar a página)
+
+        api.put(endereco, values)
+            .then ( () =>{
+                navigate('/administrar');
+            })
+    }
+
+
+    function onChange(ev){
+        const {name, value} = ev.target
+        // console.log({name, value});
+
+        setValues({... values,[name]:value})
+    }
+
+    return(
+        <div className={styles.conteudo}>
+        <h1>Editar</h1>
+        <form className={styles.Form} onSubmit={onSubmit}>
+            <div className={styles.movieForm}>
+                <label htmlFor="title">Titulo</label>
+                <input type="text" id="title" name="title" onChange={onChange} />
+            </div>
+            <div className={styles.movieForm}>
+                <label htmlFor="url">Url do Filme</label>
+                <input type="text" id="url" name="url" onChange={onChange} />
+            </div>
+            <div className={styles.movieForm}>
+                <label htmlFor="image">Url/Imagem</label>
+                <input type="text" id="image" name="image" onChange={onChange} />
+            </div>
+            <div className={styles.movieForm}>
+                <label htmlFor="sinopse">Sinopse</label>
+                <input type="text" id="sinopse" name="sinopse" onChange={onChange} />
+            </div>
+            <button className={styles.Botao} type="submit">Salvar</button>
+        </form>
+        </div>
+        
+    )
+}
+
+export default Editar;
